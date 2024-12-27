39eddd10e290891178f76e2be32cbff3c0650f7e
Velimir Paleksić <80988227+velimirpaleksic@users.noreply.github.com>
2024-12-27 01:07:45 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+// Change hero background
+function heroChange() {
+    const heroImages = document.querySelectorAll('.hero-image');
+    let currentImageIndex = 0;
+
+    // Function to fade images
+    function fadeImages() {
+        heroImages[currentImageIndex].style.opacity = 0;
+        currentImageIndex = (currentImageIndex + 1) % heroImages.length;
+        heroImages[currentImageIndex].style.opacity = 1;
+    }
+
+    // Call the fadeImages
+    setInterval(fadeImages, 3000);
+}
+
 // Toggle Mobile Menu
 const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile-menu");
